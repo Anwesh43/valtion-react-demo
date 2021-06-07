c2d23de34d18b678244b199b28a89e83614c66d7
Anwesh43 <anweshthecool0@gmail.com>
2021-06-08 03:31:06 +0530

    adding start and stop method

--- src/countState.js
+++ src/countState.js
@@ -1,10 +1,22 @@
 import {proxy} from 'valtio'
 
 export const state = proxy({
-    count: 0 
+    count: 0, 
+    interval : null 
 })
 
 export const increment = () => {
     state.count++
 }
 
+
+export const start = (delay) => {
+    state.interval = setInterval(increment, delay)
+}
+
+export const stop = () => {
+    if (state.interval) {
+        clearInterval(state.interval)
+        state.interval = null 
+    }
+}
